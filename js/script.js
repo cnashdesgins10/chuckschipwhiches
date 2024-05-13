f6cb528b6d4628761b68294569abc113ec51b0b0
cnashdesgins10 <149715751+cnashdesgins10@users.noreply.github.com>
2024-05-13 14:40:22 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* ========================= typing animation ===========================*/
+var typed = new Typed(".typing", {
+    strings: [,"Delight", "High Quality","Chipwhiches", "Cookies", "Ice Cream"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop:true
+})
+
+/* ========================= Smooth Scroll ===========================*/
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 2000,
+	speedAsDuration: true
+});
